bf430b93d51b3a268e0246eba2e3638802c983e6
meaghan jennings <meaghanljennings@gmail.com>
2021-03-03 19:48:07 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,10 @@
 def roll_call_dwarves(array)
   i = 0
   array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
-end
+end 
+# prints out 7 dwarves in a numbered list 
+
+# need to iterate through array, capitalize each word & add !
 
 def summon_captain_planet(array)
   planeteer_calls = []
@@ -13,12 +16,12 @@ end
 
 def long_planeteer_calls(array)
   array.any? do |word|
-  word.length > 4
+    word.length > 4
   end
 end
 
 def find_the_cheese(array)
   array.find do |cheese|
-  cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+    cheese == "cheddar" || cheese == "gouda" || cheese ==  "camembert"
   end
 end
