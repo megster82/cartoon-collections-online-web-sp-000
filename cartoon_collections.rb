9c4c736f46099e21fdff669ec71399cc43805e89
meaghan jennings <meaghanljennings@gmail.com>
2021-03-03 19:44:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,9 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? do 
-    array.length > 4 == 0 
-  
+  array.any? do |word|
+    word.length > 4
+  end
+end
+
+def   
