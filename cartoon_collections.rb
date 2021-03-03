2477c6b64fe320b27e0c89ba59322fd1f886a53b
meaghan jennings <meaghanljennings@gmail.com>
2021-03-03 19:19:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,24 +1,12 @@
 def roll_call_dwarves(array)
   i = 0
   array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
-end
+end 
+# prints out 7 dwarves in a numbered list 
 
 def summon_captain_planet(array)
-  planeteer_calls = []
-  array.collect do |calls|
-    planeteer_calls << "#{calls.capitalize}!"
-  end
-  planeteer_calls
+  new_array = []
+  # need to change array to a string, capitalize each word and add ! and covert back #to array
+  planeteer_calls.map {|el, ind| new_array << planeteer_calls.capitalize
 end
-
-def long_planeteer_calls(array)
-  array.any? do |word|
-  word.length > 4
-  end
-end
-
-def find_the_cheese(array)
-  array.find do |cheese|
-  cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
-  end
-end
+  
